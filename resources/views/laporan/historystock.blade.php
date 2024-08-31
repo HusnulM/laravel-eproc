ea0196491c9ac78bd0cd5af7dc48596af6ce0b23
HusnulM <husnulmub@gmail.com>
2024-08-31 22:43:22 +0700

    add total value report stock history

--- resources/views/laporan/historystock.blade.php
+++ resources/views/laporan/historystock.blade.php
@@ -11,7 +11,7 @@
         <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title"></h3>
+                    <h3 class="card-title">Laporan History Stock</h3>
                     <div class="card-tools">
 
                     </div>
